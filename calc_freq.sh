aa3b91e892f7052b37a626089a3460d10625fd3b
Michelle Daya <michelle.daya@ucdenver.edu>
2016-11-11 12:35:43 -0700

    Fixed another bug in the vcftools call

--- calc_freq.sh
+++ calc_freq.sh
@@ -6,7 +6,7 @@ grep 1$ AA_pheno.txt | cut -f1 -d' ' > tmp_ids.txt
 vcftools --vcf  AA_chr2.dose.vcf \
          --keep tmp_ids.txt \
          --recode --stdout >  controls.dose.vcf
-vcftools controls.dose.vcf \
+vcftools --vcf controls.dose.vcf \
          --freq --chr 2 \
          --out AA_chr2_controls_freq
 rm controls.dose.vcf
@@ -16,7 +16,7 @@ grep 2$ AA_pheno.txt | cut -f1 -d' ' > tmp_ids.txt
 vcftools --vcf AA_chr2.dose.vcf \
          --keep tmp_ids.txt \
          --recode --stdout >  cases.dose.vcf
-vcftools cases.dose.vcf \
+vcftools --vcf cases.dose.vcf \
          --freq --chr 2 \
          --out AA_chr2_cases_freq
 rm cases.dose.vcf
