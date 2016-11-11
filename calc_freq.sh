35139cb86832b60aad13b1e3b2b8941194fa7ad9
Michelle Daya <michelle.daya@ucdenver.edu>
2016-11-11 15:50:04 -0700

    Submitting fix to create correct header line that can be read into R

--- calc_freq.sh
+++ calc_freq.sh
@@ -11,6 +11,8 @@ vcftools --vcf controls.dose.vcf \
          --out AA_chr2_controls_freq
 rm controls.dose.vcf
 rm tmp_ids.txt
+cat AA_chr2_controls_freq.frq | sed 's/{ALLELE:FREQ}/A1:F1 A2:F2/' > new_AA_chr2_controls_freq.frq
+mv new_AA_chr2_controls_freq.frq AA_chr2_controls_freq.frq
 
 grep 2$ AA_pheno.txt | cut -f1 -d' ' > tmp_ids.txt
 vcftools --vcf AA_chr2.dose.vcf \
@@ -21,3 +23,5 @@ vcftools --vcf cases.dose.vcf \
          --out AA_chr2_cases_freq
 rm cases.dose.vcf
 rm tmp_ids.txt
+cat AA_chr2_cases_freq.frq | sed 's/{ALLELE:FREQ}/A1:F1 A2:F2/' > new_AA_chr2_cases_freq.frq
+mv new_AA_chr2_cases_freq.frq AA_chr2_cases_freq.frq
