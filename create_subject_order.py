a4b8bdb5e6276c02875d825bc71cd97cb0b42d5d
Michelle Daya <michelle.daya@ucdenver.edu>
2016-11-15 13:29:25 -0700

    Added a step to ensure that the kinship matrix PLINK files have subjects in the correct order

---
+++ create_subject_order.py
@@ -0,0 +1,14 @@
+import os
+
+col_name_file=open("tmp_column_names.txt")
+out_file=open("tmp_subject_order.txt")
+
+line=col_name_file.readline()
+elements=line.strip().split()
+
+for i in range(9, len(elements)):
+    iid=elements[i].split("_")[0]
+    out_file.write(iid + " " + iid + "\n")
+
+col_name_file.close()
+out_file.close()
--- tped_binary.sh
+++ tped_binary.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-
+#Get Detroit IDs to exclude
 cut -f1,2 -d' ' /gpfs/barnes_share/dcl01_data_aniket/data/CAAPA_jhuGRAAD_BDOS_032416/kinshipmatrix/allcohort_AA_DETROIT_emmaxkin_input.tfam > detroit_ids.txt
 
+#Ensure that subjects are in the same order as in the VCF file by creating a file with the required order
+head -50 AA_chr2.dose.vcf | grep CHROM > tmp_column_names.txt
+python create_subject_order.py
+
+#Extract the kinship file
 plink --tfile /gpfs/barnes_share/dcl01_data_aniket/data/CAAPA_jhuGRAAD_BDOS_032416/kinshipmatrix/afr_am_typed_overlap_transposed \
     --extract /gpfs/barnes_share/dcl01_data_aniket/data/CAAPA_jhuGRAAD_BDOS_032416/kinshipmatrix/allcohort_kinship_markers.txt \
     --remove detroit_ids.txt \
+    --indiv-sort file tmp_subject_order.txt \
     --make-bed -out AA_king
+
+#Cleanup
+rm detroit_ids.txt
+rm tmp_subject_order.txt
+rm tmp_column_names.txt
